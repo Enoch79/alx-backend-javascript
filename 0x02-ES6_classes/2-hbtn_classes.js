ab46e1e75fd386a17865d32880f205de0d5c1128
Enoch79 <linanar8@gmail.com>
2022-12-22 23:01:48 +0000

    ES6

---
+++ 0x02-ES6_classes/2-hbtn_classes.js
@@ -0,0 +1,16 @@
+
+remote: Resolving deltas: 100% (1/1), completed with 1 local object.
+To https://github.com/Enoch79/alx-backend-javascript.git
+   8c903d5..4093d62  master -> master
+root@5dcb99b928d1:/alx-backend-javascript/0x01-ES6_promise# client_loop: send disconnect: Connection reset by peer
+
+YOUNG BRASIL@DESKTOP-3UURMH4 MINGW64 ~
+$ ssh 5dcb99b928d1@5dcb99b928d1.82b1359d.alx-cod.online
+5dcb99b928d1@5dcb99b928d1.82b1359d.alx-cod.online's password:
+root@5dcb99b928d1:/# cd alx-backend-javascript/
+root@5dcb99b928d1:/alx-backend-javascript# mkdir 0x02-ES6_classes
+root@5dcb99b928d1:/alx-backend-javascript# cd 0x02-ES6_classes/
+root@5dcb99b928d1:/alx-backend-javascript/0x02-ES6_classes# echo '0x02-ES6_classes' > README.md
+root@5dcb99b928d1:/alx-backend-javascript/0x02-ES6_classes# vi 0-classroom.js
+root@5dcb99b928d1:/alx-backend-javascript/0x02-ES6_classes# vi 1-make_classrooms.js
+
